fc4b200cf6b0aec00cdeac6c50e162ce92c4580a
Kevin ABRIOUX <kevin.abrioux@gmail.com>
2019-06-11 09:34:31 +0200

    FrogRiverOne algo

---
+++ app/src/test/java/com/perso/codibility/FrogRiverOne.kt
@@ -0,0 +1,56 @@
+package com.perso.codibility
+
+import org.junit.Assert
+import org.junit.Test
+
+class FrogRiverOne {
+
+    /**
+     * 100%
+     * Complexity O(N)
+     */
+    fun solution(X: Int, A: IntArray): Int {
+        var count = X
+        val steps = BooleanArray(X) { false }
+        A.forEachIndexed { index, value ->
+            if (!steps[value - 1]) {
+                steps[value - 1] = true
+                count -= 1
+            }
+            if (count == 0) {
+                return index
+            }
+        }
+        return -1
+    }
+
+    @Test
+    fun test1() {
+        val list = listOf(1, 3, 1, 4, 2, 3, 5, 4).toIntArray()
+        Assert.assertEquals(6, this.solution(5, list))
+    }
+
+    @Test
+    fun test2() {
+        val list = listOf(1).toIntArray()
+        Assert.assertEquals(-1, this.solution(5, list))
+    }
+
+    @Test
+    fun test3() {
+        val list = listOf(1).toIntArray()
+        Assert.assertEquals(0, this.solution(1, list))
+    }
+
+    @Test
+    fun test4() {
+        val list = listOf(1, 3, 1, 4, 2, 3, 5, 4, 7).toIntArray()
+        Assert.assertEquals(-1, this.solution(7, list))
+    }
+
+    @Test
+    fun test5() {
+        val list = listOf(1, 6, 1, 4, 2, 3, 5, 4, 7).toIntArray()
+        Assert.assertEquals(8, this.solution(7, list))
+    }
+}
--- app/src/test/java/com/perso/codibility/PermCheck.kt
+++ app/src/test/java/com/perso/codibility/PermCheck.kt
@@ -4,9 +4,18 @@ import org.junit.Assert
 import org.junit.Test
 
 class PermCheck {
+    /**
+     * 100%
+     * O(N) or O(N * log(N))
+     */
     fun solution(A: IntArray): Int {
+        val map = mutableMapOf<Int,Boolean>()
         var count = (A.size) * (A.size + 1) / 2
         A.forEach {
+            if(map[it] == true) {
+                return 0
+            }
+            map[it] = true
             count -= it
         }
         return if (count == 0) {
@@ -24,6 +33,12 @@ class PermCheck {
 
     @Test
     fun test2() {
+        val list = listOf(1, 4, 1).toIntArray()
+        Assert.assertEquals(0, this.solution(list))
+    }
+
+    @Test
+    fun test3() {
         val list = listOf(1, 4, 1).toIntArray()
         Assert.assertEquals(0, this.solution(list))
     }
