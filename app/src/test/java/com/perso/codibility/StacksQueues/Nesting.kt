0806beb22a814cf0992ef72ce69f0117572a0e6c
Kevin ABRIOUX <kevin.abrioux@gmail.com>
2019-06-24 17:39:29 +0200

    improve algorithm

---
+++ app/src/test/java/com/perso/codibility/StacksQueues/Nesting.kt
@@ -0,0 +1,58 @@
+package com.perso.codibility.StacksQueues
+
+import org.junit.Assert
+import org.junit.Test
+
+class Nesting {
+
+    /**
+     * 100%
+     * O(N)
+     */
+    fun solution(S: String): Int {
+        if (S.count() % 2 == 1)
+            return 0
+        var count = 0
+        S.forEach {
+            count += if (it == '(') {
+                1
+            } else {
+                -1
+            }
+            if (count < 0)
+                return 0
+        }
+        return if (count == 0) {
+            1
+        } else {
+            0
+        }
+    }
+
+    @Test
+    fun test1() {
+        Assert.assertEquals(1, this.solution("(()(())())"))
+    }
+
+    @Test
+    fun test2() {
+        Assert.assertEquals(0, this.solution(")(()(())()"))
+    }
+
+    @Test
+    fun test3() {
+        Assert.assertEquals(1, this.solution(""))
+    }
+
+    @Test
+    fun test4() {
+        Assert.assertEquals(0, this.solution("((("))
+    }
+
+
+    @Test
+    fun test5() {
+        Assert.assertEquals(0, this.solution("())"))
+    }
+
+}
---
+++ app/src/test/java/com/perso/codibility/StacksQueues/StoneWall.kt
@@ -0,0 +1,37 @@
+package com.perso.codibility.StacksQueues
+
+import org.junit.Assert
+import org.junit.Test
+
+class StoneWall {
+
+    fun solution(H: IntArray): Int {
+        val cubes = mutableListOf<Int>()
+        val pyramid = mutableListOf<Int>()
+        H.forEach { height ->
+            if (cubes.isEmpty()) {
+                cubes.add(height)
+            } else if (cubes.last() > height) {
+                if (!pyramid.isEmpty())
+                    pyramid.removeAt(pyramid.count() - 1)
+                if(pyramid.last() == height){
+
+                }else{
+
+                }
+                cubes.add(height)
+
+                pyramid.add(height)
+            } else if (cubes.last() < height) {
+                pyramid.add(height)
+                cubes.add(height)
+            }
+        }
+        return cubes.count()
+    }
+
+    @Test
+    fun test1() {
+        Assert.assertEquals(7, this.solution(listOf(8, 8, 5, 7, 9, 8, 7, 4, 8).toIntArray()))
+    }
+}
